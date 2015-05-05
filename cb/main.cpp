565efb905973f1f0225638477f6cd46be7178948
relund <lars@relund.dk>
2015-05-05 15:55:08 +0200

    Added codeblocks subfolder (cb)

---
+++ cb/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "../src/hmdp.h"
+
+using namespace std;
+
+int main()
+{
+//    HMDP hmdp("hmdp_");
+//    hmdp.verbose = true;
+//    //cout << hmdp.GetLog();
+//    //cout << hmdp.Print();
+//
+//    //hmdp.PolicyIte(HMDP::AverageReward, (idx)10, (idx)0, (idx)1);
+//    //hmdp.PolicyIte(HMDP::DiscountedReward, (idx)10, (idx)0, (idx)1, 0.1, 365);
+//    idx s = hmdp.GetStateSize("0");
+//    cout << s << endl;
+//    vector<flt> termV(s,0);
+//    hmdp.ValueIte(HMDP::DiscountedReward,20000,1e-03,(idx)0,(idx)1,termV,0,0.1,365);
+//    cout << hmdp.GetLog();
+
+    HMDP hmdp("ext_");
+    hmdp.verbose = true;
+    cout << hmdp.GetLog();
+    //cout << hmdp.Print();
+    hmdp.PolicyIte(HMDP::AverageReward, (idx)10, (idx)0, (idx)1);
+    cout << hmdp.GetLog();
+    //hmdp.PolicyIte(HMDP::DiscountedReward, (idx)10, (idx)0, (idx)1, 0.1, 365);
+
+    return 0;
+}
