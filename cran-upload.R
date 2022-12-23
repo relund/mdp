147fc8861ce4fd757862170f91a0ac0349cbc982
Lars Relund <junk@relund.dk>
2022-12-23 09:45:39 +0100

    Modify steps

--- cran-upload.R
+++ cran-upload.R
@@ -1,7 +1,7 @@
 devtools::spell_check()
 spelling::update_wordlist()
 devtools::check(env_vars = c(NOT_CRAN = "true"))
-#devtools::check_win_release(quiet = TRUE)  # win-builder
+devtools::check_win_release(quiet = TRUE)  # win-builder
 devtools::check_rhub(interactive = F)
 # Push files to GitHub for GitHub actions check
 devtools::release() 
